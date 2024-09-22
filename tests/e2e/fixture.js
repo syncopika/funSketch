e051120be963d4d259ee61ed7d8a2fac67eada2c
nicholas hung <nhung93@outlook.com>
2024-09-22 10:32:52 -0400

    more e2e testing

--- tests/e2e/fixture.js
+++ tests/e2e/fixture.js
@@ -4,10 +4,18 @@ fixture('test funSketch')
   // port is specified in server.js. make sure server.js is running and then run this test in a separate terminal
   .page('http://localhost:3000');
   
-test('first test', async t => {
+test('basic test', async t => {
   await t.expect(Selector('title').textContent).eql(' funSketch ');
-  
   await t.expect(Selector('h3').withText('funSketch').exists).ok();
-  
   await t.expect(Selector('#count').withText('frame: 1, layer: 1').exists).ok();
+
+  // check that toolbar section exists + instructions show
+  await t.expect(Selector('.toolbar').exists).ok();
+  await t.expect(Selector('section').withText('instructions').exists).ok();
+  
+  // check that there is a canvas + onion skin layer
+  await t.expect(Selector('.canvasArea').child().count).eql(2);
+  
+  // check that the animation timeline exists
+  await t.expect(Selector('.animationTimelineCanvas').exists).ok();
 });
